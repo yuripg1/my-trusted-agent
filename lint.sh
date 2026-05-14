598edd802824d836c6cf200ed7fa168cdd236977
Yuri Gossmann <yuripg1@gmail.com>
2026-05-14 00:44:10 -0300

    feat: add linting script

---
+++ lint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+VENV_DIR="./.venv"
+PYCACHE_DIR="./.pycache"
+python3 -m venv ${VENV_DIR}
+"${VENV_DIR}/bin/pip3" install -q -r ./requirements-dev.txt
+PYTHONPYCACHEPREFIX="${PYCACHE_DIR}" "${VENV_DIR}/bin/python3" -m mypy ./src
--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ set -eu -o pipefail
 VENV_DIR="./.venv"
 PYCACHE_DIR="./.pycache"
 python3 -m venv ${VENV_DIR}
-"${VENV_DIR}/bin/pip3" install -q -r "./requirements.txt"
-PYTHONPYCACHEPREFIX="${PYCACHE_DIR}" "${VENV_DIR}/bin/python3" "./src/main.py"
+"${VENV_DIR}/bin/pip3" install -q -r ./requirements.txt
+PYTHONPYCACHEPREFIX="${PYCACHE_DIR}" "${VENV_DIR}/bin/python3" ./src/main.py
